f036b0dd6eae231b68d9f68b75adfa65b4b14cef
Swagnik Ghosal <ghosalswagnik@gmail.com>
2023-07-30 19:34:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+var numberofbuttons=document.querySelectorAll(".drum").length
+for (var i = 0; i <numberofbuttons; i++) {
+    
+document.querySelectorAll("button")[i].addEventListener("click",function () {
+  var buttoncases=this.innerHTML;
+  switch (buttoncases) {
+    case 'w':
+      var tom1=new Audio('./sounds/tom-1.mp3');
+      tom1.play();
+      break;
+      case 'a':
+        var tom2=new Audio('./sounds/tom-2.mp3');
+        tom2.play();
+        break;
+        case 's':
+          var tom3=new Audio('./sounds/tom-3.mp3');
+          tom3.play();
+          break;
+          case 'd':
+            var tom4=new Audio('./sounds/tom-4.mp3');
+            tom4.play();
+            break;
+            case 'j':
+              var snare=new Audio('./sounds/snare.mp3');
+            snare.play();
+              break;
+              case 'k':
+                var crash =new Audio('./sounds/crash.mp3');
+               crash.play();
+                break;
+                case 'l':
+                  var kickbass=new Audio('./sounds/kick-bass.mp3');
+                  kickbass.play();
+                  break;
+    default: console.log(this.innerHTML);
+      break;
+  }
+})
+
+}
